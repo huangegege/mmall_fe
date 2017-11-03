9810f53d44911f99a230e19a5958c86a6f05fac0
huangegege <330673934@qq.com>
2017-11-03 14:00:22 +0800

    modify

--- mmall/src/page/order-confirm/address-modal.js
+++ mmall/src/page/order-confirm/address-modal.js
@@ -2,7 +2,7 @@
  * @Author: huangegege
  * @Date:   2017-11-02 17:52:34
  * @Last Modified by:   huangegege
- * @Last Modified time: 2017-11-02 19:47:27
+ * @Last Modified time: 2017-11-03 14:00:05
  */
 'use strict';
 var _mm = require('util/mm.js');
@@ -27,6 +27,26 @@ var addressModal = {
       var selectedProvince = $(this).val();
       _this.loadCities(selectedProvince);
     });
+    // 提交收货地址
+    this.$modalWrap.find('.address-btn').click(function() {
+      var receiverInfo = _this.getReceiverInfo(),
+        isUpdate = _this.option.isUpdate;
+      // 使用新地址，且验证通过
+      if (!isUpdate && receiverInfo.status) {
+        _address.save(receiverInfo.data, function(res) {
+          _mm.successTips('地址添加成功');
+          _this.hide();
+          typeof _this.option.onSuccess === 'function' && _this.option.onSuccess(res);
+        }, function(errMsg) {
+          _mm.errorTips(errMsg);
+        })
+      } else if (isUpdate && receiverInfo.status) { //更新收件人，并且验证通过
+
+      } else { // 验证不通过
+        _mm.errorTips(receiverInfo.errMsg || '好像哪里不对了～');
+      }
+    });
+    // 点击叉号或者蒙版区域，关闭弹窗
   },
   loadModal: function() {
     var addressModalHtml = _mm.renderHtml(templateAddressModal, this.option.data);
@@ -39,14 +59,43 @@ var addressModal = {
   // 加载省份信息
   loadProvince: function() {
     var provinces = _cities.getProvinces() || [],
-      $provinceSelect = this.$modalWrap.find('receiver-province');
+      $provinceSelect = this.$modalWrap.find('#receiver-province');
     $provinceSelect.html(this.getSelectOption(provinces));
   },
   // 加载城市信息
   loadCities: function(provinceName) {
     var cities = _cities.getCities(provinceName) || [],
-      $citySelect = this.$modalWrap.find('receiver-city');
+      $citySelect = this.$modalWrap.find('#receiver-city');
     $citySelect.html(this.getSelectOption(cities));
+  },
+  // 获取表单里收件人信息，并作表单的验证
+  getReceiverInfo: function() {
+    var receiverInfo = {},
+      result = {
+        status: false
+      };
+    receiverInfo.receiverName = $.trim(this.$modalWrap.find('#receiver-name').val());
+    receiverInfo.receiverProvince = this.$modalWrap.find('#receiver-province').val();
+    receiverInfo.receiverCity = this.$modalWrap.find('#receiver-city').val();
+    receiverInfo.receiverAddress = $.trim(this.$modalWrap.find('#receiver-address').val());
+    receiverInfo.receiverPhone = $.trim(this.$modalWrap.find('#receiver-phone').val());
+    receiverInfo.receiverZip = $.trim(this.$modalWrap.find('#receiver-zip').val());
+    if (!receiverInfo.receiverName) {
+      result.errMsg = '请输入收件人姓名';
+    } else if (!receiverInfo.receiverProvince) {
+      result.errMsg = '请选择收件人所在省份';
+    } else if (!receiverInfo.receiverCity) {
+      result.errMsg = '请输入收件人所在城市';
+    } else if (!receiverInfo.receiverAddress) {
+      result.errMsg = '请选择收件人详细地址';
+    } else if (!receiverInfo.receiverPhone) {
+      result.errMsg = '请输入收件人手机号';
+    } else {
+      // 所有验证都通过了
+      result.status = true;
+      result.data = receiverInfo;
+    }
+    return result;
   },
   // 获取select框的选项，输入:array， 输出:HTML
   getSelectOption: function(optionArray) {
@@ -57,7 +106,7 @@ var addressModal = {
     return html;
   },
   hide: function() {
-
+    this.$modalWrap.empty();
   }
 };
 module.exports = addressModal;
--- mmall/src/service/address-service.js
+++ mmall/src/service/address-service.js
@@ -2,7 +2,7 @@
  * @Author: huangegege
  * @Date:   2017-11-02 16:13:12
  * @Last Modified by:   huangegege
- * @Last Modified time: 2017-11-02 16:23:49
+ * @Last Modified time: 2017-11-03 11:37:46
  */
 'use strict';
 
@@ -16,6 +16,15 @@ var _address = {
       data: {
         pageSize: 50
       },
+      success: resolve,
+      error: reject
+    });
+  },
+  // 新建收件人
+  save: function(addressInfo, resolve, reject) {
+    _mm.request({
+      url: _mm.getServerUrl('/shipping/add.do'),
+      data: addressInfo,
       success: resolve,
       error: reject
     });
