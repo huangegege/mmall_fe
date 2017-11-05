dde3d16844dfcc7b310c33a0c6c8806a448f0e93
huangegege <330673934@qq.com>
2017-11-05 17:24:20 +0800

    add notice-con, copyright modify

--- mmall/src/page/common/index.js
+++ mmall/src/page/common/index.js
@@ -1,12 +1,13 @@
 /*
-* @Author: Rosen
-* @Date:   2017-05-09 16:48:55
-* @Last Modified by:   Rosen
-* @Last Modified time: 2017-05-17 21:20:30
-*/
+ * @Author: Rosen
+ * @Date:   2017-05-09 16:48:55
+ * @Last Modified by:   huangegege
+ * @Last Modified time: 2017-11-05 17:03:10
+ */
 
 'use strict';
 
 require('./layout.css');
 require('node_modules/font-awesome/css/font-awesome.min.css');
 require('./footer/index.css');
+require('./notice-con/index.css');
